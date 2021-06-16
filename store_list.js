c01442a40ff76a16913932a6db291d3d7e44014d
s2019w33 <s2019w33@e-mirim.hs.kr>
2021-06-17 03:49:38 +0900

    store list page finish - ver1

---
+++ store_list.js
@@ -0,0 +1,18 @@
+const init = () => {
+    const placesDiv = document.querySelector(".places");
+    let places;
+    places = fetch('./place.json')
+        .then(res => res.json())
+        .then(res => {
+            places = res
+
+            places.map((place, index) => {
+                let div = document.createElement('div');
+                div.setAttribute("class", "place");
+                div.innerHTML = `<a href="${place.site}"><img src="./img_store.png" width=20 height=20><span style="color: #3273a8">${place.title}</span><br/><span>${place.info}</span></a>`
+                placesDiv.appendChild(div);
+            })
+        })
+}
+
+window.onload = () => init();
