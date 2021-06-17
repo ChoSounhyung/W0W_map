5a43530a6dc752f5a3bfa73c58500982058a7e6c
s2019w38 <s2019w38@e-mirim.hs.kr>
2021-06-17 11:49:16 +0900

    get place info when click them

--- store_list.js
+++ store_list.js
@@ -4,15 +4,34 @@ const init = () => {
   places = fetch('./place.json')
       .then(res => res.json())
       .then(res => {
-          places = res
+        places = res
 
-          places.map((place, index) => {
-              let div = document.createElement('div');
-              div.setAttribute("class", "place");
-              div.innerHTML = `<a href="${place.site}"><img src="./img_store.png" width=20 height=20><span style="color: #3273a8">${place.title}</span><br/><span>${place.info}</span></a>`
-              placesDiv.appendChild(div);
-          })
-      })
+        
+        places.map((place, index) => {
+            let div = document.createElement('div');
+            div.setAttribute("class", "place");
+            div.innerHTML = `<img src="./img_store.png" width=20 height=20><span style="color: #3273a8">${place.title}</span><br/><span>${place.info}</span><br/><a href="${place.site}"><span>${place.site}</span></a>`
+            placesDiv.appendChild(div);
+        })
+
+        var placeDiv = $('.places').children('.place');
+        placeDiv.map((index) => {
+         
+            placeDiv[index].addEventListener('click', () => {
+                getInfo(index);
+            })
+        })
+        
+    })
+}
+
+const getInfo = (index) => {
+    places = fetch('./place.json')
+    .then(res => res.json())
+    .then(res => {
+      places = res
+      console.log(places[index]);
+    })
 }
 
 window.onload = () => init();
